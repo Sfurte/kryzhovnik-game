4eab1ea974f4bbc6256dfa8a985a1cf338f4f918
DENIS-777-BARSILA <rety.18@bk.ru>
2025-04-23 22:30:16 +0500

    Graph

---
+++ Assets/Scenes Resurses/Game res/Buttons/Add/Button_Add.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Add : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+        
+    }
+}
---
+++ Assets/Scenes Resurses/Game res/Buttons/Pause/Pause_Script.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause_Script : MonoBehaviour
+{
+
+    [SerializeField] private GameObject pausePanel; 
+    private bool isPaused = false;
+
+
+
+
+    public void OnButtonClick()
+    {
+        Time.timeScale = 0f;
+        if (pausePanel != null)
+            pausePanel.SetActive(true);
+        Debug.Log("Игра на паузе");
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        if (pausePanel != null)
+            pausePanel.SetActive(false);
+        Debug.Log("Игра продолжается");
+    }
+}
